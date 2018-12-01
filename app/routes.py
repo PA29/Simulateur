4e33899bd54c660bc7ec98698b4dd30bb8a9ff49
TanguyHouget <tanguy.houget@laposte.net>
2018-12-01 21:42:05 +0100

    Réecriture de la fonction render_template pour charger le html et le css ensemble

--- app/routes.py
+++ app/routes.py
@@ -5,10 +5,10 @@ Created on Tue Nov 20 17:31:24 2018
 @author: Tanguy
 """
 
-from flask import render_template
+from functions import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('resultats.html')
+    return render_template('resultats')
