edeb8b74054ce4bf0a3caacdee1db22cd269e8c2
TanguyHouget <tanguy.houget@laposte.net>
2018-12-01 19:01:38 +0100

    Correction des bugs

--- app/routes.py
+++ app/routes.py
@@ -5,9 +5,10 @@ Created on Tue Nov 20 17:31:24 2018
 @author: Tanguy
 """
 
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return 'Hello World !'
+    return render_template('resultats.html')
