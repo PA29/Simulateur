a73021947e7ad27fdc3e17bb77ec2ec1426e3f8e
TanguyHouget <tanguy.houget@laposte.net>
2019-01-08 18:04:34 +0100

    Modification du format des données du réseau pour coller avec le modèle elec

--- app/static/reseau.js
+++ app/static/reseau.js
@@ -1,12 +1,18 @@
 var ctx;
 var images = {};
 
-var reseau = {
+/*var reseau = {
 	transfo: {x: 50, y: 10},
 	connections: [{x: 50, y: 50}, {x: 25, y: 75}, {x: 75, y: 75}],
 	lines: [[null, 0], [0, 1], [0, 2]],
 	bus: [{type: "transfo", x: 50, y: 90, line: 1, position: 0.9}]
-};
+};*/
+
+var reseau = {
+	bus: [{x: 50, y: 20}, {x: 50, y: 50}, {x: 25, y: 75}, {x: 75, y: 75}],
+	lines: [{bus1: 0, bus2: 1, length: 10}, {bus1: 1, bus2: 2, length: 10}, {bus1: 1, bus2: 3, length: 10}],
+	images: [{type: 'transfo', x: 50, y: 10, bus: 0}, {type: 'transfo', x: 50, y: 90, bus: 2}]
+}
 
 var pointSize = 3;
 
@@ -31,43 +37,24 @@ function resizeCanvas() {
 function drawReseau(reseau) {
 	drawImage('transfo', reseau.transfo);
 
-	for (let conn of reseau.connections) {
-		drawConnection(conn);
-	}
-
-	for (let line of reseau.lines) {
-		drawLine(line);
-	}
-
 	for (let bus of reseau.bus) {
 		drawBus(bus);
 	}
+
+	for (let line of reseau.lines) {
+		drawStroke(reseau.bus[line.bus1], reseau.bus[line.bus2]);
+	}
+
+	for (let image of reseau.images) {
+		drawImage(image);
+		drawStroke(image, reseau.bus[image.bus]);
+	}
 }
 
-function drawConnection(position) {
+function drawBus(position) {
 	ctx.beginPath();
 	ctx.arc(absoluteX(position.x), absoluteY(position.y), pointSize, 0, 2 * Math.PI);
 	ctx.fill();
-}
-
-function drawLine(line) {
-	let bus1 = (line[0] == null) ? reseau.transfo : reseau.connections[line[0]], bus2 = reseau.connections[line[1]];
-	drawStroke(bus1, bus2);
-}
-
-function drawBus(bus) {
-	drawImage(bus.type, bus);
-
-	if (Object.keys(bus).includes('connection')) {
-		drawStroke(bus, reseau.connections[bus.connection]);
-	}
-	else if (Object.keys(bus).includes('line')) {
-		let line = reseau.lines[bus.line];
-		let bus1 = reseau.connections[line[0]], bus2 = reseau.connections[line[1]];
-		let busLine = {x: bus.position * bus2.x + (1 - bus.position) * bus1.x, y: bus.position * bus2.y + (1 - bus.position) * bus1.y};
-		drawConnection(busLine);
-		drawStroke(bus, busLine);
-	}
 }
 
 function drawStroke(position1, position2) {
@@ -77,13 +64,13 @@ function drawStroke(position1, position2) {
 	ctx.stroke();
 }
 
-function drawImage(name, position) {
+function drawImage(image) {
 	let im = new Image();
 	im.onload = function() {
-		ctx.drawImage(im, absoluteX(position.x) - im.width / 2, absoluteY(position.y) - im.height / 2);
+		ctx.drawImage(im, absoluteX(image.x) - im.width / 2, absoluteY(image.y) - im.height / 2);
 	}
-	im.src = '/static/' + name + '.png';
-	images[name] = im;
+	im.src = '/static/' + image.type + '.png';
+	images[image.type] = im;
 }
 
 function absoluteX(x) {
