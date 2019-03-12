411aa09d1737d57b54140b69033f5c2211e1fac9
unknown <claire.meunier@ecl17.ec-lyon.fr>
2019-03-12 17:38:34 +0100

    Graphe 1 fonctionnel

--- app/static/chart.js
+++ app/static/chart.js
@@ -2,15 +2,52 @@ var imgName = "back_chart";
 var pointSizeChart = 4;
 var x_axis = [2,5,8,11,14,17,20,23,26,29,32,35,38,41,44,47,50,53,56,59,62,65,68,71];
 var y_axis = [];
+var rawdata = [5.2,3,7,5,9,2.6,7,8,5,4,2,8,6,5]
 
 
 function createChart () {
-	canvasChart = new Canvas('chart_0');
-	canvasChart.drawImage(imgName, {x:100,y:150, corner:true}, 400);
-	var i;
-	for (i=0; i<24; i++) {
-		Point({x:x_axis[i],y:y_axis[i]})
-	}
+	// 1) Création d'un objet jsGraphDisplay
+	var graph = new jsGraphDisplay(/*{
+		axe: {
+			arrow: "",
+			thickness: "",
+			color: "",
+			x: {
+				title: "Heure",
+				list: "",
+				min: "0",
+				max: "32",
+				step: "2",
+				textDisplayEvery: "",
+				textSize: "",
+				textColor: ""
+			},
+			y: {
+				title: "",
+				list: "",
+				min: "0",
+				max: "35",
+				step: "2",
+				textDisplayEvery: "",
+				textSize: "",
+				textColor: ""
+			}
+		}*/);
+	// 2) Ajout des données
+	graph.DataAdd({
+		data: [
+			[4, 21],
+			[8, 23],
+			[12, 26],
+			[16, 25],
+			[20, 20],
+			[24, 22],
+			[28, 27],
+			[32, 35]
+		]
+	});
+	// 3) Affichage du résultat
+	graph.Draw('chart_0');
 }
 
 //Conversion des résultats bruts en coordonnées en %
@@ -18,6 +55,7 @@ function y_axis(rawdata){
 	var i;
 	for (i=0; i<24; i++){
 		y_axis[i] = rawdata[i]*100/Math.max(rawdata)
+		console.log(y_axis)
 	}
 }
 
